0136723fa0de4f28d830f39d39022b1bfe8604f2
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 06:57:43 +0700

    #14 - Add SpringMailConfig

---
+++ src/main/java/com/hendisantika/springbootthymeleaftemplateemail/config/SpringMailConfig.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootthymeleaftemplateemail.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-template-email
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/20
+ * Time: 06.57
+ */
+@Configuration
+public class SpringMailConfig {
+}
