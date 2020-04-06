772d595a5661a19bf6359b70f34c8f4ae4e989b3
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 06:58:17 +0700

    #15 - Add mailSender in SpringMailConfig

--- src/main/java/com/hendisantika/springbootthymeleaftemplateemail/config/SpringMailConfig.java
+++ src/main/java/com/hendisantika/springbootthymeleaftemplateemail/config/SpringMailConfig.java
@@ -1,6 +1,11 @@
 package com.hendisantika.springbootthymeleaftemplateemail.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +18,24 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SpringMailConfig {
+    @Bean
+    public JavaMailSender mailSender() {
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.host"); //change or read from properties
+        mailSender.setPort(25);
+        mailSender.setProtocol("smtp");
+
+        // create java mail properties
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", false);
+        mailProperties.put("mail.smtp.starttls.enable", true);
+        mailProperties.put("mail.smtp.timeout", 5000);
+        mailProperties.put("mail.smtp.connectiontimeout", 5000);
+        mailSender.setJavaMailProperties(mailProperties);
+
+        return mailSender;
+
+    }
 }
