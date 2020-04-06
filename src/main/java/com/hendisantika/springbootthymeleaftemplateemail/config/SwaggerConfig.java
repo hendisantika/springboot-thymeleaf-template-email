378ac51a88c5a7bf751543fc251a93709e66224b
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 06:55:03 +0700

    #11 - Add addResourceHandlers in SwaggerConfig

--- src/main/java/com/hendisantika/springbootthymeleaftemplateemail/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springbootthymeleaftemplateemail/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootthymeleaftemplateemail.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -53,5 +54,17 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
                 .validatorUrl(null).build();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 
 }
