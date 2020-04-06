1e45a066f100046f409d66a023c62eeb848e1f7c
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 06:54:29 +0700

    #9 - Add apiMonitor in SwaggerConfig

--- src/main/java/com/hendisantika/springbootthymeleaftemplateemail/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springbootthymeleaftemplateemail/config/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package com.hendisantika.springbootthymeleaftemplateemail.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -18,4 +23,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @ComponentScan("com.hendisantika.springbootthymeleaftemplateemail")
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket apiMonitor() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.hendisantika.springbootthymeleaftemplateemail"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
 }
