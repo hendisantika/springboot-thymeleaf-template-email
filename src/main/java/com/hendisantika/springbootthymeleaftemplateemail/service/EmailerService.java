a3aa004dfe0fc078fcf6cb915eb8ac2f61ddc19b
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-07 07:01:02 +0700

    #23 - Add sendEmail in EmailerService

--- src/main/java/com/hendisantika/springbootthymeleaftemplateemail/service/EmailerService.java
+++ src/main/java/com/hendisantika/springbootthymeleaftemplateemail/service/EmailerService.java
@@ -1,11 +1,18 @@
 package com.hendisantika.springbootthymeleaftemplateemail.service;
 
+import com.hendisantika.springbootthymeleaftemplateemail.dto.EmailDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,5 +41,42 @@ public class EmailerService {
 
     @Autowired
     TemplateEngine fileTemplateEngine;
+
+    /**
+     * Send emails using templates in Emailer/Templates/ directory
+     *
+     * @param EmailDto
+     * @return EmailDto
+     * @throws MessagingException
+     * @throws IOException
+     */
+    public EmailDto sendEmail(EmailDto emailDto) throws MessagingException, IOException {
+
+        // Prepare the evaluation context
+        Context ctx = prepareContext(emailDto);
+
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = this.mailSender.createMimeMessage();
+        // Prepare message using a Spring helper
+        MimeMessageHelper message = prepareMessage(mimeMessage, emailDto);
+
+        // Create the HTML body using Thymeleaf
+        String htmlContent = this.fileTemplateEngine.process(emailDto.getTemplateName(), ctx);
+        message.setText(htmlContent, true /* isHtml */);
+        emailDto.setEmailedMessage(htmlContent);
+
+        log.info("Processing email request: " + emailDto.toString());
+
+        message = prepareStaticResources(message, emailDto);
+
+        // Send mail
+        this.mailSender.send(mimeMessage);
+
+        this.fileTemplateEngine.clearTemplateCache();
+
+        return emailDto;
+
+    }
+
 
 }
